797efcba030420e00783a0bbde11564e38b0f473
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2021-12-01 08:12:18 +0000

    Factored out the pair filtering and counting for day 1.
    Signed-off-by: Abdullah, Fawwaz <crashmacompilers@gmail.com>

--- src/aoc2021/days/day1.clj
+++ src/aoc2021/days/day1.clj
@@ -4,12 +4,15 @@
 (def day1-ints
   (util/get-parsed-input-lines "day1" #(Integer/parseInt %1)))
 
+(defn ^Long count-snd-lager [pairs]
+  (count (filter (fn [[a b]] (> b a)) pairs)))
+
 (defn part1 []
   (let [zipped (map vector day1-ints (drop 1 day1-ints))]
-    (count (filter (fn [[a b]] (> b a)) zipped))))
+    (count-snd-lager zipped)))
 
 (defn part2 []
   (let [zipped (map vector day1-ints (drop 1 day1-ints) (drop 2 day1-ints))
         summed (map #(apply + %1) zipped)
         paired (map vector summed (drop 1 summed))]
-    (count (filter (fn [[a b]] (> b a)) paired))))
+    (count-snd-lager paired)))
