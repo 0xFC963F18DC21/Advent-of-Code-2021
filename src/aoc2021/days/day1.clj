f18b0090702e53d7d399a2142813b3609a52320d
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2021-12-01 08:05:03 +0000

    Factored out the input fetching for day 1.
    Signed-off-by: Abdullah, Fawwaz <crashmacompilers@gmail.com>

--- src/aoc2021/days/day1.clj
+++ src/aoc2021/days/day1.clj
@@ -1,14 +1,15 @@
 (ns aoc2021.days.day1
   (:require [aoc2021.util.input :as util]))
 
+(def day1-ints
+  (util/get-parsed-input-lines "day1" #(Integer/parseInt %1)))
+
 (defn part1 []
-  (let [asInts (util/get-parsed-input-lines "day1" #(Integer/parseInt %1))
-        zipped (map vector asInts (drop 1 asInts))]
+  (let [zipped (map vector day1-ints (drop 1 day1-ints))]
     (count (filter (fn [[a b]] (> b a)) zipped))))
 
 (defn part2 []
-  (let [asInts (util/get-parsed-input-lines "day1" #(Integer/parseInt %1))
-        zipped (map vector asInts (drop 1 asInts) (drop 2 asInts))
+  (let [zipped (map vector day1-ints (drop 1 day1-ints) (drop 2 day1-ints))
         summed (map #(apply + %1) zipped)
         paired (map vector summed (drop 1 summed))]
     (count (filter (fn [[a b]] (> b a)) paired))))
