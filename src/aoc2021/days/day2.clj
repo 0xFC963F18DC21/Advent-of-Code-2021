35832f99238183e551112218f7dffaeb6af0e506
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2021-12-02 11:51:04 +0000

    Refactored Day 2 to use reductions.
    Signed-off-by: Abdullah, Fawwaz <crashmacompilers@gmail.com>

--- src/aoc2021/days/day2.clj
+++ src/aoc2021/days/day2.clj
@@ -9,29 +9,28 @@
       (let [[cmd n] (str/split s #" ")]
         (list cmd (Integer/parseInt n))))))
 
-(defn get-path-sum [[[cmd n] & rest] [dist depth]]
-  (let [fwd (lazy-seq (list (+ dist n) depth))
-        dp+ (lazy-seq (list dist (+ depth n)))
-        dp- (lazy-seq (list dist (- depth n)))]
-    (case cmd
-      "forward" (if rest (recur rest fwd) fwd)
-      "down" (if rest (recur rest dp+) dp+)
-      "up" (if rest (recur rest dp-) dp-))))
+(defn new-sub-data [[dist depth] [cmd n]]
+  (case cmd
+    "forward" (list (+ dist n) depth)
+    "down" (list dist (+ depth n))
+    "up" (list dist (- depth n))))
 
-(defn get-path-sum-aimed [[[cmd n] & rest] [dist depth aim]]
-  (let [fwd (lazy-seq (list (+ dist n) (+ depth (* aim n)) aim))
-        dp+ (lazy-seq (list dist depth (+ aim n)))
-        dp- (lazy-seq (list dist depth (- aim n)))]
-    (case cmd
-      "forward" (if rest (recur rest fwd) fwd)
-      "down" (if rest (recur rest dp+) dp+)
-      "up" (if rest (recur rest dp-) dp-))))
+(defn new-aimed-sub-data [[dist depth aim] [cmd n]]
+  (case cmd
+    "forward" (list (+ dist n) (+ depth (* aim n)) aim)
+    "down" (list dist depth (+ aim n))
+    "up" (list dist depth (- aim n))))
+
+(defn get-path-sum [commands]
+  (reduce new-sub-data '(0 0) commands))
+
+(defn get-path-sum-aimed [commands]
+  (reduce new-aimed-sub-data '(0 0 0) commands))
 
 (defn part1 []
-  (let [[ds dp] (get-path-sum (doall day2-pairs) '(0 0))]
+  (let [[ds dp] (get-path-sum (doall day2-pairs))]
     (* ds dp)))
 
 (defn part2 []
-  (let [[ds dp & _] (get-path-sum-aimed (doall day2-pairs) '(0 0 0))]
+  (let [[ds dp] (get-path-sum-aimed (doall day2-pairs))]
     (* ds dp)))
-
