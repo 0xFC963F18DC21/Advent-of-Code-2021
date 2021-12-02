a3937c67ab747fe847f91e66a43eba933e238146
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2021-12-02 14:11:27 +0000

    Combined both new-sub-data functions.
    Signed-off-by: Abdullah, Fawwaz <crashmacompilers@gmail.com>

--- src/aoc2021/days/day2.clj
+++ src/aoc2021/days/day2.clj
@@ -9,23 +9,22 @@
       (let [[cmd n] (str/split s #" ")]
         (list cmd (Integer/parseInt n))))))
 
-(defn new-sub-data [[dist depth] [cmd n]]
-  (case cmd
-    "forward" (list (+ dist n) depth)
-    "down" (list dist (+ depth n))
-    "up" (list dist (- depth n))))
-
-(defn new-aimed-sub-data [[dist depth aim] [cmd n]]
-  (case cmd
-    "forward" (list (+ dist n) (+ depth (* aim n)) aim)
-    "down" (list dist depth (+ aim n))
-    "up" (list dist depth (- aim n))))
+(defn new-sub-data [[dist depth aim] [cmd n]]
+  (if aim
+    (case cmd
+      "forward" (list (+ dist n) (+ depth (* aim n)) aim)
+      "down" (list dist depth (+ aim n))
+      "up" (list dist depth (- aim n)))
+    (case cmd
+      "forward" (list (+ dist n) depth)
+      "down" (list dist (+ depth n))
+      "up" (list dist (- depth n)))))
 
 (defn get-path-sum [commands]
   (reduce new-sub-data '(0 0) commands))
 
 (defn get-path-sum-aimed [commands]
-  (reduce new-aimed-sub-data '(0 0 0) commands))
+  (reduce new-sub-data '(0 0 0) commands))
 
 (defn part1 []
   (let [[ds dp] (get-path-sum (doall day2-pairs))]
