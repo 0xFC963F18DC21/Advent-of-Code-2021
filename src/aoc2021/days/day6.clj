464c8e979f9c5408a9b6d1625156e0fa6bc7ab57
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2021-12-06 06:48:23 +0000

    Converted all Longs to BigInts in Day 6 to allow even bigger numbers.
    Signed-off-by: Abdullah, Fawwaz <crashmacompilers@gmail.com>

--- src/aoc2021/days/day6.clj
+++ src/aoc2021/days/day6.clj
@@ -4,7 +4,7 @@
 
 (defn fish-to-map [fishes]
   (reduce
-    (fn [acc fish] (merge-with + acc {fish (count (filter #(= % fish) fishes))}))
+    (fn [acc fish] (merge-with + acc {fish (bigint (count (filter #(= % fish) fishes)))}))
     {}
     '(0 1 2 3 4 5 6 7 8)))
 
@@ -14,7 +14,7 @@
        (filter #(and (not= \newline %) (not= \return %)))
        (apply str)
        (#(str/split % #","))
-       (map #(Long/parseLong %))
+       (map bigint)
        (fish-to-map)))
 
 (defn simulate [fishes days]
@@ -31,7 +31,7 @@
     fishes))
 
 (defn count-fish [fishes]
-  (reduce (fn [acc [_ c]] (+ acc c)) 0 fishes))
+  (reduce (fn [acc [_ c]] (+ acc c)) 0N fishes))
 
 (defn part1 []
   (-> day6-fish
