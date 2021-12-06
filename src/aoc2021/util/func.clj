4e413e567dcd7f3b0f6f1a6d16a22b1b16908690
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2021-12-06 16:44:39 +0000

    Removed unneeded helpers from aoc2021.util.func.
    Signed-off-by: Abdullah, Fawwaz <crashmacompilers@gmail.com>

--- src/aoc2021/util/func.clj
+++ src/aoc2021/util/func.clj
@@ -3,19 +3,7 @@
 
 (defn flip
   "Flips the argument order of a two-argument function."
-  [f] #(f %2 %1))
-
-(defn compose
-  "Takes two one-argument functions and composes them together, where given
-  functions f and g, applies them as if it was done as (f (g arg))."
-  [f g] #(f (g %1)))
-
-(defn dbl-cmp
-  "Takes a one-argument function and a two-argument function and applies them in
-  such a way that the output of the latter is fed as the input to the former.
-  I.e. if f is the one-argument function and g is the two-argument function,
-  they are applied in the following manner: (f (g arg1 arg2))."
-  [f g] #(f (g %1 %2)))
+  [f & args] (apply f (reverse args)))
 
 (defmacro ^FunctionalInterface afi1
   "Turns this Clojure function into a 1-arity Java SAM FunctionalInterface object."
