e731ff3a860163ea70849dcfe4b2c316d039bd4e
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2021-12-13 05:46:07 +0000

    Added solution for Day 13.
    Signed-off-by: Abdullah, Fawwaz <crashmacompilers@gmail.com>

--- src/aoc2021/days/day13.clj
+++ src/aoc2021/days/day13.clj
@@ -1,3 +1,52 @@
 (ns aoc2021.days.day13
   (:require [aoc2021.util.input :as inp]
+            [clojure.java.io :as io]
             [clojure.string :as strs]))
+
+(defn generate-info [[dots folds]]
+  (list
+    (reduce
+      #(let [[sx sy] (strs/split %2 #",")] (conj %1 (list (Long/parseLong sx) (Long/parseLong sy))))
+      #{}
+      dots)
+    (reduce
+      #(let [[_ axis v] (re-find #".+([xy])=(.+)" %2)] (conj %1 (list (first axis) (Long/parseLong v))))
+      []
+      folds)))
+
+(def day13-info
+  (->> "day13"
+       (inp/get-input-lines)
+       (filter #(not (empty? %)))
+       (partition-by #(< (count %) 10))
+       (generate-info)
+       (lazy-seq)))
+
+(defn fold-once [grid [axis v]]
+  (case axis
+    \x (reduce (fn [cur [x y :as p]] (if (< x v) (conj cur p) (conj cur (list (- v (- x v)) y)))) #{} grid)
+    \y (reduce (fn [cur [x y :as p]] (if (< y v) (conj cur p) (conj cur (list x (- v (- y v)))))) #{} grid)
+    :Invalid-axis!))
+
+(defn write-grid [grid]
+  (let [max-x (reduce max 0 (map first grid))
+        max-y (reduce max 0 (map second grid))
+        rx    (range 0 (inc max-x))
+        ry    (range 0 (inc max-y))
+        sb    (StringBuilder.)]
+    (io/delete-file "outputs/day31-part2.txt" :Does-not-exist-yet!)
+    (doseq [y ry
+            x rx]
+      (if (contains? grid (list x y)) (.append sb \#) (.append sb \space))
+      (if (= x max-x) (.append sb \newline)))
+    (spit "outputs/day13-part2.txt" (.toString sb))))
+
+(defn part1 []
+  (let [[grid [first & _]] day13-info]
+    (count (fold-once grid first))))
+
+(defn part2 []
+  (let [[grid folds] day13-info]
+    (->> grid
+         (#(reduce fold-once % folds))
+         (write-grid))))
