3bf4d87bb32afc0091a41dc8a291b6290353777c
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2021-12-15 08:38:27 +0000

    Finally, it works!
    Signed-off-by: Abdullah, Fawwaz <crashmacompilers@gmail.com>

--- src/aoc2021/days/day15.clj
+++ src/aoc2021/days/day15.clj
@@ -1,16 +1,9 @@
 (ns aoc2021.days.day15
-  (:require [aoc2021.util.input :as inp]
-            [clojure.string :as strs]))
+  (:require [aoc2021.util.input :as inp])
+  (:import (java.util PriorityQueue Comparator)))
 
 (def day15-grid
   (->> "day15"
-       (inp/get-input-lines)
-       (map #(apply vector %))
-       (mapv #(mapv (fn [n] (Long/parseLong (str n))) %))
-       (apply vector)))
-
-(def day15-small-grid
-  (->> "day15-small"
        (inp/get-input-lines)
        (map #(apply vector %))
        (mapv #(mapv (fn [n] (Long/parseLong (str n))) %))
@@ -27,21 +20,54 @@
     not-found
     (aget arr y x)))
 
-(defn risk-sum [grid]
-  (let [sy     (count grid)
-        sx     (count (first grid))
-        sumarr (to-array-2d grid)]
+(defn risk-sum [grid exxxxpand]
+  (let [dummy   2147483647
+        sy      (count grid)
+        sx      (count (first grid))
+        usx     (* sx (if exxxxpand 5 1))
+        usy     (* sy (if exxxxpand 5 1))
+        add     (fn [x y] (+ (long (/ x sx)) (long (/ y sy))))
+        nrm     (fn [a]   (mod a sx))
+        sub     (fn [v] (if (> v 9) (- v 9) v))
+        gg      (fn [x y]
+                  (let [x' (nrm x)
+                        y' (nrm y)
+                        v  ((grid y') x')
+                        a  (+ (add x y) v)]
+                    (sub a)))
+        sumarr  (to-array-2d (repeat usy (repeat usx dummy)))
+        p-queue (PriorityQueue.
+                  (reify
+                    Comparator
+                    (compare [_ [x1 y1 v1] [x2 y2 v2]]
+                      (let [dx1   (- (dec usx) x1)
+                            dy1   (- (dec usy) y1)
+                            dx2   (- (dec usx) x2)
+                            dy2   (- (dec usy) y2)]
+                        (- (+ dx1 dy1 v1) (+ dy2 dx2 v2))))))]
+    (.offer p-queue (list 0 0 0))
     (aset sumarr 0 0 0)
-    (doseq [[x y] (diagonal-iteration sy)]
-      (if (and (= x 0) (= y 0))
-        nil
-        (let [pl  (safe-2d-aget sumarr y (dec x) sy sx Long/MAX_VALUE)
-              pu  (safe-2d-aget sumarr (dec y) x sy sx Long/MAX_VALUE)
-              cur (safe-2d-aget sumarr y x sy sx Long/MAX_VALUE)
-              mn  (min pl pu)]
-          (aset sumarr y x (+ mn cur)))))
-    (println (java.util.Arrays/deepToString sumarr))
-    (aget sumarr (dec sy) (dec sx))))
+    (while (not (.isEmpty p-queue))
+      (let [[x y _] (.poll p-queue)
+            go (fn [x' y' cr']
+                 (let [p (safe-2d-aget sumarr y' x' usy usx nil)
+                       s (lazy-seq (list (+ cr' (gg x' y'))))]
+                   (if (and (not (nil? p)) (> p (first s)))
+                     (do
+                       (aset sumarr y' x' (first s))
+                       (.offer p-queue (list x' y' (first s)))))))
+            cr (aget sumarr y x)]
+        (if (and (= x (dec usx)) (= y (dec usy)))
+          (.clear p-queue)
+          (do
+            (go (dec x) y cr)
+            (go x (dec y) cr)
+            (go (inc x) y cr)
+            (go x (inc y) cr)))))
+    (aget sumarr (dec usy) (dec usx))))
 
 (defn part1 []
-  (risk-sum day15-small-grid))
+  (risk-sum day15-grid false))
+
+(defn part2 []
+  (risk-sum day15-grid true))
